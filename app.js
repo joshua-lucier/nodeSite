97f52a7ef6206c2979f649bc3210d3b93f2e29a9
Joshua Lucier <Joshua.Lucier@gmail.com>
2016-03-21 15:48:17 -0400

    Squared away pg package install

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
+var settings = require('./settings');
+var pg = require('pg');
+
+pg.connect(settings.conString, function(err, client, done)
+{
+	if(err)
+	{
+		return console.error('error fetching client from pool',err);
+	}
+});
 
 app.get('/',function(req,res){
 	res.send('Hello World!');
 });
 
-app.listen(3000, function(){
+app.listen(settings.port, function(){
 	console.log('Example app listening on 3000');
 
 });
