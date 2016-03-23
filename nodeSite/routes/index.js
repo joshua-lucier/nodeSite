0e6f1a02363d424cbc038b8edda9106e73d326c0
Joshua Lucier <Joshua.Lucier@gmail.com>
2016-03-23 11:30:30 -0400

    Working on blog login

--- nodeSite/routes/index.js
+++ nodeSite/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/profile', function(req, res, next){
+	res.render('profile', { title: 'Josh\'s Profile'});
+});
+
+router.get('/blog', function(req, res, next){
+	res.render('blog', { title: 'Blog'});
+});
+
+router.get('/resume', function(req, res, next){
+	res.render('resume', { title: 'Resume'});
+});
+
+router.get('/contact', function(req, res, next){
+	res.render('contact', { title: 'Contact'});
+});
+
+router.get('/login', function(req, res, next){
+	res.render('login', { title: 'Login'});
 });
 
 module.exports = router;
